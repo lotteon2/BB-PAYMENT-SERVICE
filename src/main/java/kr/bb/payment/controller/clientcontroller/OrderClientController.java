bce6725b9b4082875161c6f2f35323d21684dbf1
binarywoo27 <blu_mountain@naver.com>
2023-12-26 21:56:27 +0900

    💚 의존성 수정 및 CommonResponse로 변경

--- src/main/java/kr/bb/payment/controller/clientcontroller/OrderClientController.java
+++ src/main/java/kr/bb/payment/controller/clientcontroller/OrderClientController.java
@@ -1,13 +1,12 @@
 package kr.bb.payment.controller.clientcontroller;
 
-import bloomingblooms.response.SuccessResponse;
+import bloomingblooms.response.CommonResponse;
 import java.time.LocalDateTime;
 import kr.bb.payment.dto.request.KakaopayApproveRequestDto;
 import kr.bb.payment.dto.request.KakaopayReadyRequestDto;
 import kr.bb.payment.dto.response.KakaopayReadyResponseDto;
 import kr.bb.payment.service.KakaopayService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,26 +20,16 @@ public class OrderClientController {
   private final KakaopayService kakaopayService;
 
   @PostMapping("/ready")
-  public SuccessResponse<KakaopayReadyResponseDto> payReady(
+  public CommonResponse<KakaopayReadyResponseDto> payReady(
       @RequestBody KakaopayReadyRequestDto readyRequestDto) {
 
-    KakaopayReadyResponseDto responseDto = kakaopayService.kakaoPayReady(readyRequestDto);
-
-    return SuccessResponse.<KakaopayReadyResponseDto>builder()
-        .data(responseDto)
-        .message(HttpStatus.OK.name())
-        .build();
+    return CommonResponse.success(kakaopayService.kakaoPayReady(readyRequestDto));
   }
 
   @PostMapping("/approve")
-  public SuccessResponse<LocalDateTime> payApprove(
+  public CommonResponse<LocalDateTime> payApprove(
       @RequestBody KakaopayApproveRequestDto approveRequestDto) {
 
-    LocalDateTime paymentDateTime = kakaopayService.kakaoPayApprove(approveRequestDto);
-
-    return SuccessResponse.<LocalDateTime>builder()
-        .data(paymentDateTime)
-        .message(HttpStatus.OK.name())
-        .build();
+    return CommonResponse.success(kakaopayService.kakaoPayApprove(approveRequestDto));
   }
 }
